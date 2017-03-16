7826833202dc1ece7ce67afd3b0367445eb5ee65
mhanifamrizal <mhanifamrizal@gmail.com>
2017-03-16 08:10:16 +0700

    rubah folder

--- Praktikum1/Exercise3/bootstrap.sh
+++ Praktikum1/Exercise3/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
-apt-get -y install ruby-full
-
+wget http://ftp.ruby-lang.org/pub/ruby/2.4/ruby-2.4.0.tar.gz
+tar -xzvf ruby-2.4.0.tar.gz
+cd ruby-2.4.0/
+./configure
+make
+sudo make install
+ruby -v
--- Praktikum1/Exercise4/bootstrap.sh
+++ Praktikum1/Exercise4/bootstrap.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-apt-get update
-wget http://ftp.ruby-lang.org/pub/ruby/2.4/ruby-2.4.0.tar.gz
-tar -xzvf ruby-2.4.0.tar.gz
-cd ruby-2.4.0/
-./configure
-make
-sudo make install
+sudo apt-get update
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+
+sudo apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm -y install 2.4.0
+rvm use 2.4.0 --default
 ruby -v
+
+gem -y install bundler
